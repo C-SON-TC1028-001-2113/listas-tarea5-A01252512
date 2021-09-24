d2a27a691077196f9d3bfefd6c98e149bf63ff92
A01252512 <a01252512@itesm.mx>
2021-09-24 22:42:35 +0000

    V1

--- assignments/08SinDuplicados/src/exercise.py
+++ assignments/08SinDuplicados/src/exercise.py
@@ -1,8 +1,22 @@
 def main():
     #escribe tu código abajo de esta línea
+    lista = []
+    listaSinDuplicados = []
+    n = int(input())
 
+    if n > 0:
+        for i in range(n):
+            valor = input()
+            if valor not in lista:
+                listaSinDuplicados.append(valor)
+            lista.append(valor)
+            
+        print(lista)
+        print(listaSinDuplicados)
+    else:
+        print("Error")
 
-    pass
+    
 
 if __name__=='__main__':
     main()
